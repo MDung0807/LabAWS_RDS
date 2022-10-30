403541492dc3005e74ac0d5ff3abb5e307ba14ed
do dung <dominhdung21082002@gmail.com>
2022-10-30 10:13:21 +0700

    update

--- insertAWSDB_AuroraMySQL.py
+++ insertAWSDB_AuroraMySQL.py
@@ -4,7 +4,7 @@ import MySQLdb
 
 #Tạo kết nối với RDS Aurora MySQL và thêm dữ liệu vào
 
-conn = MySQLdb.connect(host='database-aurora.cluster-c62t9l3xga8j.us-east-1.rds.amazonaws.com', user='admin', passwd='lab-password', db='covid19_aurora', port=3306)
+conn = MySQLdb.connect(host='database-arora.cluster-ckb4im6mqwqe.us-east-1.rds.amazonaws.com', user='admin', passwd='12345678', db='covid19_aurora', port=3306)
 print('Open DB successfully')
 ut.insert(conn)
 ut.select(conn)
